64353bcae9b37afb439f0f7117a2542528544ac3
cloudwithanne <annieussang@gmail.com>
2024-12-20 11:47:10 +0100

    Submission of my AltSchool's Bash Assignment

---
+++ anne.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+who_am_i() {
+  echo "Hello, I am Anne Usang, a cloud engineering student!"
+}
+
+fullname() {
+  echo "My name is Anne Usang"
+}
+
+bio() {
+  echo "My Bio: I am an AWS Certified Solutions Architect. I am one is passionate about solving problems especially within the African ecosystem. I am knowledgeable in building cloud architectures, scripting and automations. I love to speak and share my knowledge with others."
+}
+
+year_in_tech() {
+  echo "I have been in tech for about 2 years."
+}
+
+dream_job() {
+  echo "Cloud Consultant with any of the MAANG companies"
+}
+
+goals() {
+  echo "Become vast in cloud technlogies and build cutting-edge solutions across board"
+}
+ 
